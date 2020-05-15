86d3e72a3ffd44e74ca8428d551de2ca5cc6d0df
leon945 <243156362@qq.com>
2020-05-15 22:36:46 +0800

    f

--- movie.plus.user.js
+++ movie.plus.user.js
@@ -10,7 +10,7 @@
 // @require        https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js
 // @require        https://cdn.bootcss.com/jqueryui/1.12.1/jquery-ui.min.js
 // @match          https://movie.douban.com/subject/*
-// @version        200422
+// @version        200515
 // ==/UserScript==
 
 const myScriptStyle = document.createElement("style");
